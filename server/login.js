557729c320ab9b340e29a09aac5ce3506fc0a245
AurisSolucoes <caiotav.lima19@gmail.com>
2024-10-23 17:48:27 -0300

    Update login.js

--- server/login.js
+++ server/login.js
@@ -3,7 +3,7 @@
   function loginTest(){
     var email = document.getElementById("user-email");          
     if (email.value == "caiotav.lima21@gmail.com") {
-      window.location.href = "../web/main-page.html"
+      window.location.href = "../web/main-page.html?uid=onai03tnoaID"
       
     }
   }
