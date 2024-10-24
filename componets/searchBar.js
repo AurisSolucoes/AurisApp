aa9ff8aabd29994e7b68b294ce1b2cb1cb5ce8c3
AurisSolucoes <caiotav.lima19@gmail.com>
2024-10-24 08:22:08 -0300

    Update searchBar.js

--- componets/searchBar.js
+++ componets/searchBar.js
@@ -6,7 +6,7 @@
     searchBox.addEventListener("focus", () => {
       subtitleBox.style.fontSize = "0.8em"
       subtitleBox.style.transition = "all .3s"
-      subtitleBox.style.marginTop = "-15px"
+      subtitleBox.style.marginTop = "-12px"
     })
     searchBox.addEventListener("blur", () => {
       if(searchBox.value == ""){
